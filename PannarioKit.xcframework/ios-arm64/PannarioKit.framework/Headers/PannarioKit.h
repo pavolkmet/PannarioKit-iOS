d228835fe85fb913cc4fe134e7c1a0c8ddc4cd3d
Pavol Kmet <pavol.kmet@goodrequest.com>
2020-06-06 11:43:08 +0200

    Added PannarioKit.xcframework.

---
+++ PannarioKit.xcframework/ios-arm64/PannarioKit.framework/Headers/PannarioKit.h
@@ -0,0 +1,19 @@
+//
+//  PannarioKit.h
+//  PannarioKit
+//
+//  Created by Pavol Kmeť on 30/01/2020.
+//  Copyright © 2020 Pannario, s.r.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pannario.
+FOUNDATION_EXPORT double PannarioVersionNumber;
+
+//! Project version string for Pannario.
+FOUNDATION_EXPORT const unsigned char PannarioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pannario/PublicHeader.h>
+
+
